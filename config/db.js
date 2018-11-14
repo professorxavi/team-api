deb2b89e1988a506800718087d94ff1295bf0a49
X <xavier@tauntaun.net>
2018-11-13 22:10:28 -0600

    .gitignore fix

--- config/db.js
+++
@@ -1,6 +0,0 @@
-const config = require('./config.json');
-const dbUrl = 'mongodb://' + config.user + ':' + config.pw + '@' + config.address + '/' + config.db;
-
-module.exports = {
-  url: dbUrl
-};
