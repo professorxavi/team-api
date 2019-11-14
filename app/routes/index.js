45d72c2866bba65dc6a24c7fdc1be8824711c754
X <xavier@tauntaun.net>
2019-11-13 22:07:27 -0600

    route to get active team

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,7 @@
 const partyRoutes = require('./party_routes');
+const teamRoutes = require('./team_routes');
 
 module.exports = (app, db, pokedex) => {
   partyRoutes(app, db, pokedex);
+  teamRoutes(app, db, pokedex);
 };
